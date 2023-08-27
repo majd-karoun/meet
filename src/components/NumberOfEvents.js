aa7f0da49cadf53b796a523245a1d6192ff13f0b
Majd Karoun <107414000+majd-karoun@users.noreply.github.com>
2023-08-27 18:46:01 +0200

    testing and func. for NOE input added

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,17 +1,11 @@
 const NumberOfEvents = ({ setCurrentNOE, setErrorAlert }) => {
-    const handleInputChanged = (event) => {
-      const value = event.target.value;
+   
   
-      if (isNaN(value)) {
-        setErrorAlert('value is not a number');
-      } else if (value > 50) {
-        setErrorAlert('maximum value is 50');
-      } else if (value <= 0) {
-        setErrorAlert('minimum value is 1');
-      } else {
-        setErrorAlert('');
-        setCurrentNOE(value);
-      }
+  const handleInputChanged = (event) => {
+      const value = event.target.value;
+      setCurrentNOE(value);
+  
+     
     };
   
     return (
